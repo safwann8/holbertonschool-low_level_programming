2c8a9d54d6ee3faae4db1e83602a6c609d43d702
safwann8 <safoulefou83@gmail.com>
2025-10-23 14:09:07 +0000

    adding program 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main pied
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+ {
+    prinf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+ }
+ 
