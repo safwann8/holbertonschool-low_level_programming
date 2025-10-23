f6b5e61636923c2db63961898a2289180a1041ac
safwann8 <safoulefou83@gmail.com>
2025-10-23 15:08:44 +0000

    adding

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - chocolat
-*
-* Return: Always 0 (Success)
-*/
+ * main - chocolat
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("Size of a char: %lu byte(s)\n", sizeof(char));
