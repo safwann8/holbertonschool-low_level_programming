effd9e56d40e11a1b1c99a7063c6dbef3ea36233
Safwann8 <safoulefou83@gmail.com>
2025-10-24 12:33:28 +0000

    adding program 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
 */
 int main(void)
 {
-    char letter = 'a';
-    while(letter <='z')
-    {
-        if (letter != 'e' && letter != 'q')
-        putchar (letter);
-        letter++;
-    }
+	char letter = 'a';
+	while(letter <='z')
+	{
+		if (letter != 'e' && letter != 'q')
+		putchar (letter);
+		letter++;
+	}
 
-    putchar ('\n');
-    return (0);
+	putchar ('\n');
+	return (0);
 }
