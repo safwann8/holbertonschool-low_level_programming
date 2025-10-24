d91516b7d1fb8618083472157343705f9b73d8a0
Safwann8 <safoulefou83@gmail.com>
2025-10-24 13:56:38 +0000

    adding program 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@ int main(void)
     int n;
     for (n = 0; n < 16; n++)
     {
-        if (n < 10);
-        putchar(n = n + '0')
+        if (n < 10)
+        putchar(n = n + '0');
 
         else
         putchar(n - 10 + 'a');
