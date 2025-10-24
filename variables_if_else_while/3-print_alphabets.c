de108523698825e8d0512969cefb6cc45a45780c
Safwann8 <safoulefou83@gmail.com>
2025-10-24 10:14:34 +0000

    adding program 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	char letter;
 
-    letter = 'a';
+	letter = 'a';
 	while (letter <= 'z')
 	{
 		putchar(letter);
 		letter++;
 	}
 	letter = 'A';
-    while (letter <= 'Z')
-    {
-        putchar(letter);
-        letter++;
-    }
-    
-    putchar('\n');
-    return (0);
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	
+	putchar('\n');
+	return (0);
 }
