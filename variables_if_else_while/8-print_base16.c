a93825e43ac107572b7c29b83f6d0a88145023d5
Safwann8 <safoulefou83@gmail.com>
2025-10-24 13:55:49 +0000

    adding

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
         if (n < 10);
         putchar(n = n + '0')
 
-        else (n - 10; n = 'a'; n < 'f'; n++)
+        else
         putchar(n - 10 + 'a');
     }
     putchar('\n');
