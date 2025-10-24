8431c80c4fc4c616e194edd98c60b4089b6cd121
Safwann8 <safoulefou83@gmail.com>
2025-10-24 08:37:22 +0000

    adding program 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+* main - Determine si le nombre est positif, négatif ou est égal à zero
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if(n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if(n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        print("%d is negative\n", n);
+    }
+
+    return (0);
+}
