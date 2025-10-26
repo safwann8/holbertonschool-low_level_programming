31251efb7c2d636d8269b7b90a23d4406a5f3515
Safwann8 <Safoulefou83@gmail.com>
2025-10-26 23:15:52 +0000

    adding program 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Écrivez un programme qui affiche _putchar, suivi d'une nouvelle ligne.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('a');
+	_putchar('\n');
+
+	return (0)
+}
