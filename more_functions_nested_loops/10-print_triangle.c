763e9d7fafa76288c41aacad75bc38fcc226ecdb
Safwann8 <Safoulefou83@gmail.com>
2025-10-27 15:51:58 +0000

    adding program 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -18,20 +18,20 @@ void print_triangle(int size)
 	}
 
 	else
-    {
-        for (i = 1; i <= size; i++)
-	    {
-		    for (j = size - i; j > 0; j--)
-		    {
-			    _putchar(' ');
-		    }
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
 
-		    for (k = 0; k < i; k++)
-		    {
-			    _putchar('#');
-		    }
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
 
-		    _putchar('\n');
-        }
+			_putchar('\n');
+		}
 	}
 }
