5ddd0f7dcae2aaabb1e70cc225a7feb5ea0c2b33
Safwann8 <safoulefou83@gmail.com>
2025-10-27 11:02:03 +0000

    adding program 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,18 @@
 
 void more_numbers(void)
 {
-    int n;
-    int m;
+	int n;
+	int m;
 
-    for (m = 0; m < 10; m++)
-    {
-        for (n = 0; n <= 14; n++)
-        {
-            if (n >= 10)
-            _putchar((n / 10) + '0');
-            _putchar((n % 10) + '0');
-            
-        }
-        _putchar('\n');
-    }
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			
+		}
+		_putchar('\n');
+	}
 }
