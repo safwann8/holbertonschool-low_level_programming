6713e155a5374ddf6336339aa7e0412c5b850cfc
Safwann8 <Safoulefou83@gmail.com>
2025-10-28 15:50:12 +0000

    adding program 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,13 @@
 
 void print_rev(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    i++;
+	while (s[i] != '\0')
+	i++;
 
-    while (i--)
-        _putchar(s[i]);
+	while (i--)
+		_putchar(s[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
