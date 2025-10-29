9946b81200dd029ffb050238010bf6e2ce50ec88
Safwann8 <safoulefou83@gmail.com>
2025-10-29 14:32:13 +0000

    adding program 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,27 +13,27 @@
 
 void puts_half(char *str)
 {
-    int len = 0;
-    int n, i;
+	int len = 0;
+	int n, i;
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    if (len % 2 == 0)
-    {
-        n = len / 2;
-    }
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
 
-    else
-    {
-        n = (len + 1) / 2;
-    }
-    
-    for (i = n; i < len; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
