23f6671635922ab50a342a9c364806fa48f5ea2f
Safwann8 <safoulefou83@gmail.com>
2025-10-30 16:18:03 +0000

    adding prpgram 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,19 +16,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-    i++;
+	while (dest[i] != '\0')
+	i++;
 
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    dest[i] = '\0';
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
