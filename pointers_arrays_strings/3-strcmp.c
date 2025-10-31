a4a1dd3cbd2f91a5672d146de9401333674d64e5
Safwann8 <safoulefou83@gmail.com>
2025-10-31 10:59:31 +0000

    adding program 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,24 +10,26 @@
  *   - un nombre négatif si s1 est inférieure à s2,
  *   - 0 si les deux chaînes sont identiques,
  *   - un nombre positif si s1 est supérieure à s2.
- * Le comportement est identique à celui de la fonction de comparaison de chaine du
+ * Le comportement est identique à celui de la fonction de
+ * comparaison de chaine du
  * standard C.
  *
- * Return: Un entier négatif, nul ou positif selon le résultat de la comparaison.
+ * Return: Un entier négatif, nul ou positif selon le résultat
+ * de la comparaison.
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-   while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
 	if (s1[i] > s2[i])
 	{
 		return (s1[i] - s2[i]);
-	} 
+	}
 	else if (s1[i] == s2[i])
 	{
 		return (0);
