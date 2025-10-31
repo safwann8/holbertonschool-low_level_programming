9c9623ec6bd415a3dc7064457aa01368ada6034a
Safwann8 <safoulefou83@gmail.com>
2025-10-31 09:37:11 +0000

    adding program 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -17,15 +17,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0' && i < n)
-    {
-        dest[i] = src[i]; i++;
-    }
-    if (i < n)
-    {
-        dest[i] = '\0'; i++;
-    }
-    return (dest);
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i]; i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
