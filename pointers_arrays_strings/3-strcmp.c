07a58ffcf79bba34908eaa21e1f175c634ab8036
Safwann8 <safoulefou83@gmail.com>
2025-10-31 10:52:16 +0000

    adding program 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,22 +19,21 @@
 int _strcmp(char *s1, char *s2)
 {
     int i = 0;
-    int j = 0;
 
    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
     {
         i++;
     }
-    if (s1[i] > s2[j])
+    if (s1[i] > s2[i])
     {
-        return (s1[i] - s2[j]);
+        return (s1[i] - s2[i]);
     } 
-    else if (s1[i] == s2[j])
+    else if (s1[i] == s2[i])
     {
         return (0);
     }
     else
     {
-        return (s2[j] - s1[i]);
+        return (s2[i] - s1[i]);
     }
 }
