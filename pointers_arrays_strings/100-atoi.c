3388c87a919d1e42089a36b57f7da740b332113a
Safwann8 <safoulefou83@gmail.com>
2025-10-31 09:40:41 +0000

    adding program 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,27 +14,27 @@
 
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    unsigned int result = 0;
-    int started = 0;
+	int i = 0;
+	int sign = 1;
+	unsigned int result = 0;
+	int started = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        sign *= -1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		sign *= -1;
 
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            started = 1;
-            result = (result * 10) + (s[i] - '0');
-        }
-        else if (started)
-        break;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			result = (result * 10) + (s[i] - '0');
+		}
+		else if (started)
+		break;
 
-        i++;
-    }
-    if (sign < 0)
-    return (-result);
-    return (result);
+		i++;
+	}
+	if (sign < 0)
+	return (-result);
+	return (result);
 }
