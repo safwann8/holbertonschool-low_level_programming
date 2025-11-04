54fa9297f1b2989a67ec6c4a0a7a29121625f097
Safwann8 <safoulefou83@gmail.com>
2025-11-04 10:34:36 +0000

    adding program 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,26 +10,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i = 0, j;
-    int match;
+	unsigned int i = 0, j;
+	int match;
 
-    while (s[i] != '\0')
-    {
-        match = 0;
-        j = 0;
-        while (accept[j] != '\0')
-        {
-            if (s[i] == accept[j])
-            {
-                match = 1;
-                break;
-            }
-            j++;
-        }
-        if (match == 0)
-            return i;
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		match = 0;
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				break;
+			}
+			j++;
+		}
+		if (match == 0)
+			return i;
+		i++;
+	}
 
-    return i;
+	return i;
 }
