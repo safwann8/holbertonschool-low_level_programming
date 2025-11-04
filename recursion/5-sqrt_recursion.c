135c28e4ba1ad0f07b38419bb671c35153cddb2b
Safwann8 <safoulefou83@gmail.com>
2025-11-04 15:13:29 +0000

    adding program 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calcule la racine carrée naturelle d'un nombre
+ * @n: nombre dont on cherche la racine carrée
+ *
+ * Description:
+ * Cette fonction retourne la racine carrée entière (naturelle) de n en utilisant la récursion.
+ * - Si n est négatif, elle retourne -1.
+ * - Si n n'a pas de racine carrée naturelle (i.e., aucun entier i tel que i*i == n),
+ *   elle retourne -1.
+ * - Sinon, elle retourne l'entier i tel que i*i == n.
+ * 
+ * Return: racine carrée naturelle de n, ou -1 si n n'a pas de racine naturelle
+ */
+
+int _sqrt_recursion(int n)
+{
+    int i = n;
+
+    if (n < 0)
+    {
+        return (-1);
+    }
+
+    else if (i * i == n)
+    {
+        return (-1);
+    }
+
+    else
+    {
+        return (_sqrt_helper(n, i + 1));
+    }
+}
