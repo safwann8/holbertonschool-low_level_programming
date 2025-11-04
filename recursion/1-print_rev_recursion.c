141eeddf1d8b76afb1fa780a7273e9d1100eb2ef
Safwann8 <safoulefou83@gmail.com>
2025-11-04 13:50:38 +0000

    adding program 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - affiche une chaîne en sens inverse
+ * @s: pointeur vers la chaîne
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+        return;
+
+    _print_rev_recursion(s + 1);
+    putchar(*s);
+}
