7a6fb6b39e2366864c43c6d3a92f9d64c387f242
safoulefou83@gmail.com <Safwann8>
2025-11-05 15:22:53 +0000

    adding program 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - affiche une chaîne de caractères suivie d'un saut de ligne
@@ -9,10 +8,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 
-	putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
