ac729e212b18ae0ff84ee65956bab2f66657a353
safoulefou83@gmail.com <Safwann8>
2025-11-05 15:37:45 +0000

    adding program 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,12 +9,12 @@
  */
 int _sqrt_helper(int n, int i)
 {
-    if (i * i > n)
-        return (-1);
-    else if (i * i == n)
-        return (i);
-    else
-        return (_sqrt_helper(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (_sqrt_helper(n, i + 1));
 }
 
 /**
@@ -25,8 +25,8 @@ int _sqrt_helper(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
+	if (n < 0)
+		return (-1);
 
-    return (_sqrt_helper(n, 0));
+	return (_sqrt_helper(n, 0));
 }
