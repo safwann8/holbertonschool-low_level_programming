6e2e1352610042c9dae2da0c84dc73d372e5a4f9
Safwann8 <safoulefou83@gmail.com>
2025-11-06 16:33:04 +0000

    adding program 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -7,28 +7,13 @@
  * @argc: nombre total d'arguments passés au programme
  * @argv: tableau contenant les arguments sous forme de chaînes de caractères
  *
- * Description:
- * Ce programme prend exactement deux arguments en ligne de commande,
+ * Description: Ce programme prend exactement deux arguments en ligne de commande,
  * les convertit en entiers, puis affiche le résultat de leur multiplication.
+ * Si le programme reçoit exactement deux arguments, il affiche leur produit.
+ * Sinon, il affiche "Error" et retourne 1.
  *
- * - Si le programme reçoit **exactement deux arguments** :
- *     il les convertit avec `atoi()` et affiche
- * leur produit suivi d’un saut de ligne.
- *
- * - Si le programme ne reçoit **pas deux arguments** :
- *     il affiche "Error" suivi d’un saut de ligne et retourne 1.
- *
- * Exemples :
- *   - ./mul 2 3        → affiche 6
- *   - ./mul 2 -3       → affiche -6
- *   - ./mul 245 3245342 → affiche 795108790
- *   - ./mul             → affiche "Error"
- *
- * Return:
- *   - 0 en cas de succès
- *   - 1 en cas d’erreur (nombre d’arguments incorrect)
+ * Return: 0 en cas de succès, 1 en cas d’erreur (nombre d’arguments incorrect)
  */
-
 int main(int argc, char *argv[])
 {
 	int a, b, result;
