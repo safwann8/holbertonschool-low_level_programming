a53864111d3d1e0c039bd43689670f943b308ec8
Safwann8 <safoulefou83@gmail.com>
2025-11-06 09:13:10 +0000

    adding program 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,9 +5,12 @@
  * @n: nombre dont on veut calculer le factoriel
  *
  * Description:
- * Cette fonction retourne le factoriel du nombre n en utilisant la récursion.
- * - Si n est inférieur à 0, la fonction retourne -1 pour indiquer une erreur.
- * - Si n est égal à 0, le résultat est 1 (cas de base du factoriel).
+ * Cette fonction retourne le factoriel du nombre n 
+ *en utilisant la récursion.
+ * - Si n est inférieur à 0, la fonction retourne -1 
+ *pour indiquer une erreur.
+ * - Si n est égal à 0, le résultat est 1
+ * (cas de base du factoriel).
  * - Sinon, elle retourne n multiplié par le factoriel de (n - 1).
  *
  * Return: le factoriel de n, ou -1 si n est négatif
