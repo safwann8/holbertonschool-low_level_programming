4c9f8f86979d94b0ade216a3e00601da685fa98e
Safwann8 <safoulefou83@gmail.com>
2025-11-06 09:15:53 +0000

    adding program 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - cherche récursivement la racine carrée naturelle d'un nombre
+ * _sqrt_helper - cherche récursivement la
+ * racine carrée naturelle d'un nombre
  * @n: le nombre dont on cherche la racine
  * @i: le candidat actuel pour la racine
  *
- * Return: la racine carrée naturelle, ou -1 si elle n'existe pas
+ * Return: la racine carrée naturelle,
+ * ou -1 si elle n'existe pas
  */
 int _sqrt_helper(int n, int i)
 {
