b9640c0cbedac11e124c511b72a9d1ab032ab888
Safwann8 <safoulefou83@gmail.com>
2025-11-07 22:47:09 +0000

    adding program 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	unsigned int i, j;
 
 	if (needle[0] == '\0')
-		return haystack;
+		return (haystack);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
@@ -26,5 +26,5 @@ char *_strstr(char *haystack, char *needle)
 			return (haystack + i);
 	}
 
-	return NULL;
+	return (NULL);
 }
