e1d6bd8b24c0cb12902264906a084830cbb1b03b
Safwann8 <safoulefou83@gmail.com>
2025-11-07 22:48:39 +0000

    adding program 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
- * _is_prime_helper - aide à déterminer si un nombre est premier
+ * is_prime_number - vérifie si un nombre est premier
+ * @n: le nombre à tester
+ *
+ * Return: 1 si n est premier, 0 sinon
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (_is_prime_helper(n, 2));
+}
+
+/**
+ * _is_prime_helper - aide à vérifier si un nombre est premier
  * @n: nombre à tester
- * @i: diviseur en cours de test
+ * @i: diviseur courant
  *
  * Return: 1 si n est premier, 0 sinon
  */
@@ -14,18 +28,4 @@ int _is_prime_helper(int n, int i)
 	if (n % i == 0)
 		return (0);
 	return (_is_prime_helper(n, i + 1));
-}
-
-/**
- * is_prime_number - détermine si un entier est un nombre premier
- * @n: l'entier à tester
- *
- * Description:
- * Retourne 1 si n est un nombre premier, 0 sinon.
- */
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (_is_prime_helper(n, 2));
 }
