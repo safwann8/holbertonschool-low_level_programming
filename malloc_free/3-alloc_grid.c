14babcaa7434729e46a2d58dd8ed9d4fc052fe9e
Safwann8 <safoulefou83@gmaul.com>
2025-11-13 11:13:53 +0000

    adding program 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,34 +2,16 @@
 #include <stdlib.h>
 
 /**
- * Ce programme teste la fonction alloc_grid qui crée un tableau
- * à deux dimensions (une grille) d'entiers.
+ * alloc_grid - alloue une grille (tableau 2D) d'entiers initialisée à 0
+ * @width: nombre de colonnes
+ * @height: nombre de lignes
  *
- * La fonction main commence par appeler alloc_grid(6, 4)
- * pour créer une grille composée de 4 lignes et 6 colonnes.
- * 
- * La fonction alloc_grid doit :
- * - Allouer dynamiquement la mémoire nécessaire pour stocker
- *   un tableau d'entiers de 4 lignes sur 6 colonnes.
- * - Initialiser chaque élément de la grille à 0.
- * - Retourner NULL si la largeur ou la hauteur est inférieure
- *   ou égale à 0, ou si l’allocation mémoire échoue.
+ * Description: crée dynamiquement un tableau de pointeurs vers des lignes,
+ *              puis pour chaque ligne alloue un tableau
+ *              d'entiers initialisé à 0.
+ *              Si width ou height <= 0 ou si malloc échoue, retourne NULL.
  *
- * Ensuite, le programme utilise la fonction print_grid()
- * pour afficher le contenu de la grille. 
- * Comme elle vient d’être créée, tous les éléments sont à 0.
- *
- * Puis, deux cases précises de la grille sont modifiées :
- *  - grid[0][3] reçoit la valeur 98
- *  - grid[3][4] reçoit la valeur 402
- *
- * Enfin, la grille est réaffichée, montrant les deux valeurs
- * modifiées tandis que le reste reste à 0.
- *
- * Ce programme permet donc de vérifier que :
- * - la mémoire a bien été allouée pour un tableau à deux dimensions,
- * - les éléments sont bien accessibles et modifiables,
- * - et que la fonction alloc_grid fonctionne correctement.
+ * Return: pointeur vers le tableau 2D (int **), ou NULL si erreur
  */
 
 int **alloc_grid(int width, int height)
