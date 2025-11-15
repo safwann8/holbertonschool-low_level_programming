cc2be3f0454ec37092ffcdff549b5ae80f45c7fa
Safwann8 <safoulefou83@gmail.com>
2025-11-15 21:35:54 +0000

    adding program DOG

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure describing a dog
+ * @name: pointer to the dog's name
+ * @age: age of the dog
+ * @owner: pointer to the owner's name
+ *
+ * Description: Defines a dog with a name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif
