8fe605c84efa1b675160cd999114da6d26c188c1
Safwann8 <safoulefou83@gmail.com>
2025-11-20 14:55:41 +0000

    adding 3-get_op_func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,7 +2,8 @@
 #include <stddef.h>
 
 /**
- * get_op_func - Sélectionne la fonction correspondant à l’opérateur donné
+ * get_op_func - Sélectionne la fonction correspondant
+ * à l’opérateur donné
  * @s: symbole de l'opérateur (+, -, *, /, %)
  *
  * Return: un pointeur vers la fonction correspondante,
@@ -10,22 +11,22 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (*(ops[i].op) == *s && s[1] == '\0')
-            return (ops[i].f);
-        i++;
-    }
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
