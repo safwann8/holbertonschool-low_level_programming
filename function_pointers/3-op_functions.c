bc714bce5ffd6f87559b15ee80b0377cae7e4837
Safwann8 <safoulefou83@gmail.com>
2025-11-20 14:49:59 +0000

    add

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,12 +1,5 @@
 #include "3-calc.h"
 
-/**
- * op_add - Additionne deux entiers
- * @a: premier entier
- * @b: second entier
- *
- * Return: la somme de a et b
- */
 int op_add(int a, int b)
 {
     return (a + b);
