e310c5df944e9dd302e01fc0103b91cdf708cad6
Safwann8 <safoulefou83@gmail.com>
2025-11-20 14:41:41 +0000

    main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Programme principal d’une calculatrice simple
+ * @argc: nombre d’arguments
+ * @argv: tableau d’arguments
+ *
+ * Return: 0 toujours
+ */
+int main(int argc, char *argv[])
+{
+	int (*op_func)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_func(a, b));
+
+	return (0);
+}
