de8152d9a15af3835bd276c82d7570a3aa63cbac
Safwann8 <safoulefou83@gmail.com>
2025-11-20 10:12:00 +0000

    adding program 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointeurs.h"
+#include <stddef.h>
+
+/**
+ * print_name - imprime un nom en utilisant une fonction pointeur
+ * @name: le nom à afficher
+ * @f: pointeur vers une fonction qui prend un char * en paramètre
+ *
+ * Description: Cette fonction vérifie que @name et @f ne sont pas NULL,
+ * puis appelle la fonction pointée par @f pour afficher le nom.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	char *name;
+	void (*f)(char *)
+
+	if (name == NULL || f == NULL)
+	return;
+
+	f(name);
+}
