0e2b7d6c3d7f3d60d201f4535a2b176be5a6740d
Safwann8 <safoulefou83@gmail.com>
2025-11-21 19:08:02 +0000

    adding programme 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,28 +10,29 @@
  * Description: Cette fonction parcourt tous les paramètres variadiques
  * qui sont des chaînes de caractères et les affiche. Si @separator n'est
  * pas NULL, il est affiché entre les chaînes. Si une chaîne est NULL,
- * "(nil)" est affiché à la place. Un retour à la ligne est ajouté à la fin.
+ * "(nil)" est affiché à la place. Un retour à la ligne
+ * est ajouté à la fin.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list ap;
-    unsigned int i;
-    char *str;
+	va_list ap;
+	unsigned int i;
+	char *str;
 
-    va_start(ap, n);
+	va_start(ap, n);
 
-    for (i = 0; i < n; i++)
-    {
-        str = va_arg(ap, char *);
-        if (str == NULL)
-            printf("(nil)");
-        else
-            printf("%s", str);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 
-        if (i != n - 1 && separator != NULL)
-            printf("%s", separator);
-    }
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
 
-    printf("\n");
-    va_end(ap);
+	printf("\n");
+	va_end(ap);
 }
