331dc718a33ba469d23ff2b97f2eb9a15fdd9d78
Safwann8 <safoulefou83@gmail.com>
2025-11-21 23:02:00 +0000

    adding

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,59 +3,54 @@
 #include <stdarg.h>
 
 /**
- * print_all - Prints a variable number of arguments based on a format string.
- * @format: A constant string containing the argument types.
- *
- * Description:
- * This function parses the format string and prints each corresponding
- * argument using the correct conversion:
- *   'c' → char
- *   'i' → integer
- *   'f' → float
- *   's' → string (prints "(nil)" if the string is NULL)
- *
- * Any unknown type specifier is ignored.
- * A comma and a space are printed between valid arguments.
- *
- * The function ends by printing a newline.
- *
- * Return: Nothing.
+ * print_all - prints anything
+ * @format: list of argument types
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *str;
+	int i = 0, j;
 	char *sep = "";
+	char *str;
+
+	typedef struct printer {
+		char t;
+		void (*f)(va_list);
+	} printer_t;
+
+	void print_char(va_list a) { printf("%c", va_arg(a, int)); }
+	void print_int(va_list a) { printf("%d", va_arg(a, int)); }
+	void print_float(va_list a) { printf("%f", va_arg(a, double)); }
+	void print_string(va_list a)
+	{
+		str = va_arg(a, char *);
+		if (!str)
+			str = "(nil)";
+		printf("%s", str);
+	}
+
+	printer_t funcs[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' ||
-			format[i] == 'f' || format[i] == 's')
+		j = 0;
+		while (funcs[j].t)
 		{
-			printf("%s", sep);
-
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-
-			if (format[i] == 's')
+			if (funcs[j].t == format[i])
 			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(nil)";
-				printf("%s", str);
+				printf("%s", sep);
+				funcs[j].f(args);
+				sep = ", ";
 			}
-
-			sep = ", ";
+			j++;
 		}
 		i++;
 	}
