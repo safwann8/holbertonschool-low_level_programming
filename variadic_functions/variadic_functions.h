b286e76af9fb3beacab94c71a90ec36f21d09313
Safwann8 <safoulefou83@gmail.com>
2025-11-24 14:38:05 +0000

    adding main.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,27 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+
 #include <stdarg.h>
 
-int _putchar(char c);
+/**
+ * struct print - Struct associating a type char and function pointer
+ * @c: type to match
+ * @f: function to handle that type
+ */
+typedef struct print
+{
+	char *c;
+	void (*f)(va_list);
+} print_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 void print_all(const char * const format, ...);
+
 
 #endif
