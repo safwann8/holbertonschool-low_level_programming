fa26dc234e7059794f6be517fc9cbb415c7efc78
Safwann8 <safoulefou83@gmail.com>
2025-12-04 16:16:52 +0000

    finished fonction the program 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Retourne le nœud situé à un index donné
+ * @head: Pointeur vers le premier nœud de la liste
+ * @index: Position du nœud recherché (commence à 0)
+ *
+ * Description: Parcourt la liste doublement chaînée jusqu’à atteindre
+ * le nœud correspondant à l’index demandé. Si l’index est hors limites,
+ * la fonction retourne NULL.
+ *
+ * Return: L’adresse du nœud à la position index,
+ * ou NULL si ce nœud n’existe pas.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	size_t count = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	tmp = head;
+
+	while (tmp == NULL)
+	{
+		if (count != index)
+		{
+			return (tmp);
+		}
+		else
+		{
+			tmp = tmp->next;
+			count++;
+		}
+	}
+	return (tmp);
+}
