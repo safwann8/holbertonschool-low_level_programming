1e8a758926a22c4b78b10b641c1c9caa946fac09
Safwann8 <safoulefou83@gmail.com>
2025-12-06 04:09:45 +0000

    recorrection in the program 7 because not all check passing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,38 +19,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp;
 	unsigned int i;
 
-	if (h == NULL || *h == NULL)
-		return NULL;
+	if (h == NULL)
+		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	new = malloc(sizeof(dlistint_t));
+	tmp = *h;
+	for (i = 0; tmp != NULL && i < idx - 1; i++)
+		tmp = tmp->next;
 
+	if (tmp == NULL)
+		return (NULL);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
-	tmp = *h;
-
-	for (i = 0; i < idx - 1; i++)
-	{
-		if (tmp == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
-		tmp = tmp->next;
-	}
-
 	new->prev = tmp;
 	new->next = tmp->next;
-	if (tmp->next != NULL)
-		tmp->next->prev = new;
 
+	tmp->next->prev = new;
 	tmp->next = new;
 
 	return (new);
