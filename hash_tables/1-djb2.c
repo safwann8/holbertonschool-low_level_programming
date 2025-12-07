fe638d83297f25eac8e45ce67fff1669d39eba7b
Safwann8 <safoulefou83@gmail.com>
2025-12-07 16:47:58 +0000

    creating the program 1 and finished a functions

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Implements the djb2 hash algorithm
+ * @str: Pointer to the string to hash
+ *
+ * Return: The calculated hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
